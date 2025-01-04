419698cb6b192d5378df8c164fb08bf74257084f
Robert Wigginton <rswigginton@gmail.com>
2025-01-04 14:54:52 -0700

    achordion

--- GoNew/rules.mk
+++ GoNew/rules.mk
@@ -10,3 +10,7 @@ SPACE_CADET_ENABLE = no
 CAPS_WORD_ENABLE = yes
 
 ACHORDION_ENABLE = yes
+ifeq ($(strip $(ACHORDION_ENABLE)), yes)
+	OPT_DEFS += -DACHORDION_ENABLE
+	SRC += features/achordion.c
+endif
